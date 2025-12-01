a9ac9f70a9a529224aea6f06aa81eddbe25b8b06
Rumpel Stilskin <rozbujnik@gmial.com>
2025-12-01 23:11:00 +0100

    Using linkarchivetools

--- dbfeeds.py
+++ dbfeeds.py
@@ -1,6 +1,136 @@
+import os
+import json
 import argparse
+from pathlib import Path
+from sqlalchemy import create_engine
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from linkarchivetools import Db2Feeds, DbFilter
+from linkarchivetools.utils.reflected import ReflectedEntryTable
+from webtoolkit import OpmlPage, BaseUrl, RssPage
+
+
+def list_files_recursive(root_dir):
+    file_paths = []
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            full_path = os.path.join(root, filename)
+            file_paths.append(full_path)
+    return file_paths
+
+
+def find_opml_files(root_directory):
+    result = []
+    all_files = list_files_recursive(root_directory)
+    for file in all_files:
+        if file.endswith("opml"):
+            result.append(file)
+
+    return result
+
+
+def fetch_feed(feed):
+    url = BaseUrl(feed)
+    response = url.get_response()
+    return feed, url
+
+
+def get_feed_properties(feed, url):
+    result = {}
+    properties = url.get_properties()
+
+    result["link"] = url.get_url()
+    result["title"] = url.get_title()
+    result["description"] = url.get_description()
+    result["status_code"] = url.get_status_code()
+    result["manual_status_code"] = 0
+
+    # not null requirement
+    result["source_url"] = ""
+    result["permanent"] = False
+    result["bookmarked"] = False
+    result["contents_type"] = 0
+    result["page_rating_contents"] = 0
+    result["page_rating_visits"] = 0
+    result["page_rating_votes"] = 0
+    result["page_rating"] = 0
+
+    return result
+
+
+def process_feeds(feeds, executor, futures, table):
+    #print("Processing file {}".format(afile))
+    for feed in feeds:
+        if not table.is_entry_link(feed):
+            futures.append(executor.submit(fetch_feed, feed))
+
+    for future in as_completed(futures):
+        feed, url = future.result()
+        if type(url.get_response().get_page()) is RssPage:
+            print(f"{feed}:OK")
+            properties = get_feed_properties(feed, url)
+            new_entry_id = table.insert_json_data("linkdatamodel", properties)
+        else:
+            print(f"{feed}:NOK")
+
+
+def get_feeds(all_files):
+    result = set()
+    for f in all_files:
+        print("Processing file {}".format(f))
+        with open(f, "r") as fh:
+            data = fh.read()
+            page = OpmlPage("", contents=data)
+            feeds = set(page.get_feeds())
+            for feed in feeds:
+                result.add(feed)
+
+    return result
+
+
+def process_opmls(db_name, all_feeds):
+    engine = create_engine(f"sqlite:///{db_name}")
+    with engine.connect() as connection:
+        table = ReflectedEntryTable(engine, connection)
+
+        with ThreadPoolExecutor(max_workers=5) as executor:  # run 5 at a time
+            futures = []
+
+            process_feeds(all_feeds, executor, futures, table)
+
+
+def read_json():
+    feeds = set()
+
+    url = BaseUrl("https://raw.githubusercontent.com/rumca-js/RSS-Link-Database-2025/refs/heads/main/sources.json")
+    response = url.get_response()
+    text = response.get_text()
+    loaded = json.loads(text)
+
+    for item in loaded:
+        feeds.add(item["url"])
+
+    return feeds
+
+
+def try_adding_source_feeds(db_name):
+    engine = create_engine(f"sqlite:///{db_name}")
+    with engine.connect() as connection:
+        table = ReflectedEntryTable(engine, connection)
+
+        with ThreadPoolExecutor(max_workers=5) as executor:  # run 5 at a time
+            futures = []
+
+            all_feeds = read_json()
+            process_feeds(all_feeds, executor, futures, table)
+
+
+def process_all_opml_files(args, directory):
+    all_files = find_opml_files(directory)
+    print("Found {} files".format(len(all_files)))
+    all_feeds = get_feeds(all_files)
+    print("Found {} feeds".format(len(all_feeds)))
+    process_opmls(args.output_db, all_feeds)
 
 
 def parse():
@@ -26,6 +156,12 @@ def main():
 
     analyzer = Db2Feeds(input_db = tmp_db, output_db=args.output_db)
     analyzer.convert()
+
+    awesome_path = Path("awesome-rss-feeds-master")
+    if awesome_path.exists():
+        process_all_opml_files(args, "awesome-rss-feeds-master")
+
+    try_adding_source_feeds(args.output_db)
 
 
 main()
