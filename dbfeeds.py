3d015aa9288ee1bf8aab687838156c36c6828288
Rumpel Stilskin <rozbujnik@gmial.com>
2025-12-05 21:41:18 +0100

    Fixed processing

--- dbfeeds.py
+++ dbfeeds.py
@@ -11,6 +11,9 @@ from webtoolkit import OpmlPage, BaseUrl, RssPage
 
 
 def list_files_recursive(root_dir):
+    """
+    List files recursively
+    """
     file_paths = []
     for root, dirs, files in os.walk(root_dir):
         for filename in files:
@@ -19,27 +22,23 @@ def list_files_recursive(root_dir):
     return file_paths
 
 
-def find_opml_files(root_directory):
-    result = []
-    all_files = list_files_recursive(root_directory)
-    for file in all_files:
-        if file.endswith("opml"):
-            result.append(file)
-
-    return result
-
-
 def fetch_feed(feed):
+    """
+    Fetch a feed
+    """
     url = BaseUrl(feed)
     response = url.get_response()
     return feed, url
 
 
 def get_feed_properties(feed, url):
+    """
+    Returns feed properties
+    """
     result = {}
     properties = url.get_properties()
 
-    result["link"] = url.get_url()
+    result["link"] = feed
     result["title"] = url.get_title()
     result["description"] = url.get_description()
     result["status_code"] = url.get_status_code()
@@ -58,9 +57,13 @@ def get_feed_properties(feed, url):
     return result
 
 
-def process_feeds(feeds, executor, futures, table):
-    #print("Processing file {}".format(afile))
+def process_feeds_executor(feeds, executor, futures, table):
+    """
+    Process all feeds
+    """
     for feed in feeds:
+        print("{} Fetching...".format(feed))
+
         url = BaseUrl(url=feed)
         new_feeds = url.get_feeds()
         if len(new_feeds) > 0:
@@ -80,7 +83,31 @@ def process_feeds(feeds, executor, futures, table):
             print(f"{feed}:NOK")
 
 
-def get_feeds(all_files):
+def find_opml_files(root_directory):
+    """
+    Returns list of OPML files from directory
+    """
+    result = []
+    all_files = list_files_recursive(root_directory)
+    for file in all_files:
+        if file.endswith("opml"):
+            result.append(file)
+
+    return result
+
+
+def get_all_opml_feeds(args, directory):
+    all_files = find_opml_files(directory)
+    print("Found OPML {} files".format(len(all_files)))
+    all_feeds = get_opml_feeds(all_files)
+    print("Found OMPL {} feeds".format(len(all_feeds)))
+    return all_feeds
+
+
+def get_opml_feeds(all_files):
+    """
+    Get all feeds from all OPML files
+    """
     result = set()
     for f in all_files:
         print("Processing file {}".format(f))
@@ -94,7 +121,7 @@ def get_feeds(all_files):
     return result
 
 
-def process_opmls(db_name, all_feeds):
+def process_feeds(db_name, all_feeds):
     engine = create_engine(f"sqlite:///{db_name}")
     with engine.connect() as connection:
         table = ReflectedEntryTable(engine, connection)
@@ -102,10 +129,13 @@ def process_opmls(db_name, all_feeds):
         with ThreadPoolExecutor(max_workers=5) as executor:  # run 5 at a time
             futures = []
 
-            process_feeds(all_feeds, executor, futures, table)
+            process_feeds_executor(all_feeds, executor, futures, table)
 
 
 def read_link_database_sources():
+    """
+    Returns sources from JSON
+    """
     feeds = set()
 
     url = BaseUrl("https://raw.githubusercontent.com/rumca-js/RSS-Link-Database-2025/refs/heads/main/sources.json")
@@ -120,6 +150,9 @@ def read_link_database_sources():
 
 
 def read_infobubble_sources():
+    """
+    Returns sources from infobubble
+    """
     feeds = set()
 
     url = BaseUrl("https://raw.githubusercontent.com/wokenlex/infobubble-support/refs/heads/main/Sources/all.sources.rss.yaml")
@@ -131,29 +164,6 @@ def read_infobubble_sources():
         feeds.add(item["url"])
 
     return feeds
-
-
-
-def try_adding_source_feeds(db_name):
-    engine = create_engine(f"sqlite:///{db_name}")
-    with engine.connect() as connection:
-        table = ReflectedEntryTable(engine, connection)
-
-        with ThreadPoolExecutor(max_workers=5) as executor:  # run 5 at a time
-            futures = []
-
-            all_feeds = read_link_database_sources()
-            # other = read_infobubble_sources()
-
-            process_feeds(all_feeds, executor, futures, table)
-
-
-def process_all_opml_files(args, directory):
-    all_files = find_opml_files(directory)
-    print("Found {} files".format(len(all_files)))
-    all_feeds = get_feeds(all_files)
-    print("Found {} feeds".format(len(all_feeds)))
-    process_opmls(args.output_db, all_feeds)
 
 
 def parse():
@@ -172,26 +182,33 @@ def main():
         print("Please specify database")
         return
 
-    tmp_db = "tmp.db"
+    # tmp_db = "tmp.db"
 
-    #print(f"Filtering {args.db} entries")
+    print(f"Filtering {args.db} entries")
 
-    #filter = DbFilter(input_db = args.db, output_db = tmp_db)
-    #filter.filter_votes()
+    filter = DbFilter(input_db = args.db, output_db = tmp_db)
+    filter.filter_votes()
 
-    #print(f"{args.db} places -> feeds")
+    print(f"{args.db} places -> feeds")
 
-    #analyzer = Db2Feeds(input_db = tmp_db, output_db=args.output_db)
-    #analyzer.convert()
+    analyzer = Db2Feeds(input_db = tmp_db, output_db=args.output_db)
+    analyzer.convert()
+
+    all_feeds = []
 
     awesome_path = Path("awesome-rss-feeds-master")
     if awesome_path.exists():
-        print("Reading awesome rss feeds")
-        process_all_opml_files(args, "awesome-rss-feeds-master")
+        print("Reading awesome RSS feeds")
+        all_feeds.extend(get_all_opml_feeds(args, "awesome-rss-feeds-master"))
 
 
     print("Reading rumca-js feeds")
-    try_adding_source_feeds(args.output_db)
+    all_feeds.extend(read_link_database_sources())
+
+    print("I have {} feeds".format(len(all_feeds)))
+    print("Processing feeds")
+    process_feeds(args.output_db, all_feeds)
+    print("Processing feeds DONE")
 
 
 main()
