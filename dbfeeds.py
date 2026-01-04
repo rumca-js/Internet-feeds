61f0f76032a1cf13befab55669d7d13f303b95f8
Rumpel Stilskin <rozbujnik@gmial.com>
2026-01-04 22:43:44 +0100

    CLI: Added convert and update params

--- dbfeeds.py
+++ dbfeeds.py
@@ -198,6 +198,8 @@ def parse():
     parser.add_argument("--db", default="places.db", help="DB to be scanned")
     parser.add_argument("--output-db", default="feeds.db", help="DB to be produced")
     parser.add_argument("--remote-server", help="DB to be scanned")
+    parser.add_argument("--convert", action="store_true", help="DB to be scanned")
+    parser.add_argument("--update", action="store_true", help="DB to be scanned")
 
     args = parser.parse_args()
 
@@ -212,32 +214,34 @@ def main():
 
     tmp_db = "tmp.db"
 
-    print(f"Filtering {args.db} entries")
+    if args.convert:
+        print(f"Filtering {args.db} entries")
 
-    filter = DbFilter(input_db = args.db, output_db = tmp_db)
-    filter.filter_votes()
+        filter = DbFilter(input_db = args.db, output_db = tmp_db)
+        filter.filter_votes()
 
-    print(f"{args.db} places -> feeds")
+        print(f"{args.db} places -> feeds")
 
-    converter = Db2Feeds(input_db = tmp_db, output_db=args.output_db, read_internet_links=True, remote_server=args.remote_server)
-    converter.convert()
+        converter = Db2Feeds(input_db = tmp_db, output_db=args.output_db, read_internet_links=True, remote_server=args.remote_server)
+        converter.convert()
 
-    all_feeds = set()
+    if args.update:
+        all_feeds = set()
 
-    awesome_path = Path("awesome-rss-feeds-master")
-    if awesome_path.exists():
-        print("Reading awesome RSS feeds")
-        all_feeds.update(get_all_opml_feeds(args, "awesome-rss-feeds-master"))
+        awesome_path = Path("awesome-rss-feeds-master")
+        if awesome_path.exists():
+            print("Reading awesome RSS feeds")
+            all_feeds.update(get_all_opml_feeds(args, "awesome-rss-feeds-master"))
 
-    print("Reading rumca-js feeds")
-    all_feeds.update(read_link_database_sources())
-    print("Reading infobuble feeds")
-    all_feeds.update(read_infobubble_sources())
+        print("Reading rumca-js feeds")
+        all_feeds.update(read_link_database_sources())
+        print("Reading infobuble feeds")
+        all_feeds.update(read_infobubble_sources())
 
-    print("I have {} feeds".format(len(all_feeds)))
-    print("Processing feeds")
-    process_feeds(args.output_db, all_feeds)
-    print("Processing feeds DONE")
+        print("I have {} feeds".format(len(all_feeds)))
+        print("Processing feeds")
+        process_feeds(args.output_db, all_feeds)
+        print("Processing feeds DONE")
 
 
 main()
